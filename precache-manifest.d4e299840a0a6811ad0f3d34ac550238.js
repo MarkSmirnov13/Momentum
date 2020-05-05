ec070a8d232202052e1581c4e5010175594ace2f
MaximLukianchuk <44572418+MaximLukianchuk@users.noreply.github.com>
2020-05-05 11:23:59 +0000

    Deploying to gh-pages from  @ c82b63f95847fc4844e60dc4bef1caeb6f74673d 🚀

---
+++ precache-manifest.d4e299840a0a6811ad0f3d34ac550238.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f421a7371496ee1024747b3f6ae9457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76665e72949082a5ef2d",
+    "url": "/static/css/2.551d3f2e.chunk.css"
+  },
+  {
+    "revision": "88330e650af70ef93d73",
+    "url": "/static/css/main.100c1523.chunk.css"
+  },
+  {
+    "revision": "76665e72949082a5ef2d",
+    "url": "/static/js/2.1650352d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1650352d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88330e650af70ef93d73",
+    "url": "/static/js/main.e55b35df.chunk.js"
+  },
+  {
+    "revision": "42a2cdb64250af6bcea6",
+    "url": "/static/js/runtime-main.18615233.js"
+  },
+  {
+    "revision": "8d4b57d8ec23f4f5f7a315bca60a563b",
+    "url": "/static/media/blue-gradient-example.8d4b57d8.png"
+  },
+  {
+    "revision": "aec9037540764d4e127d09112a5319a0",
+    "url": "/static/media/blue-gradient.aec90375.jpg"
+  },
+  {
+    "revision": "4399b6c5a6316802e391ba8967ee8933",
+    "url": "/static/media/red-gradient-example.4399b6c5.png"
+  },
+  {
+    "revision": "e5917570dc087e1b79552a75f835f5f4",
+    "url": "/static/media/red-gradient.e5917570.jpg"
+  },
+  {
+    "revision": "a57be422e02e768d9ab2f679fff6ff17",
+    "url": "/static/media/violet-gradient-example.a57be422.png"
+  },
+  {
+    "revision": "16e7ac8edc6a1d38d2f508f1008512e9",
+    "url": "/static/media/violet-gradient.16e7ac8e.jpg"
+  }
+]);
